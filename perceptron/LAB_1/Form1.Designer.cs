0535c0d7ff51ae548fa372179e1090b44cc6f92a
DmitriyRod <55783050+DmitriyRod@users.noreply.github.com>
2020-11-30 14:36:16 +0300

    Add files via upload

---
+++ perceptron/LAB_1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LAB_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 231);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 258);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(252, 18);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(225, 231);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 258);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Распознать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 303);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(106, 303);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 38);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "неверно";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 402);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Перцептрон";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ perceptron/LAB_1/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace LAB_1
+{
+    public partial class Form1 : Form
+    {
+        const double theta = -1;
+        const int N = 10;
+        int[,] Xi = new int[N, N];
+        double[,] Wi = new double[N, N];
+        Bitmap bmp;
+        Bitmap b2;
+        Graphics g;
+        bool isPressed = false;
+        double y1 = 0;
+        int X;
+        int Y;
+        int X1;
+        int Y1;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void READ()
+        {
+            string str = File.ReadAllText("Inp.txt");
+            char[] separatingChars = { ' ','\r','\n','\t' }; //список знаков разделителей
+            int K = 0;
+            string[] words = str.Split(separatingChars, System.StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Wi[i, j] = Convert.ToDouble(words[K]);
+                    K++;
+                }
+            }
+        }
+        private void SAVE()
+        {
+            string str = "";
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    str += Wi[i, j].ToString() + " ";
+                }
+                str += "\r";
+            }
+            File.WriteAllText("Inp.txt",str);
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Draw()
+        {
+            Pen p = new Pen(Color.Black,2);
+            g.DrawLine(p,X1,Y1,X,Y);
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            X = e.X;
+            Y = e.Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                X1 = X;
+                Y1 = Y;
+                X = e.X;
+                Y = e.Y;
+                Draw();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            Graphics.FromImage(bmp).Clear(Color.White);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            READ();
+            //границы обрезки изображения
+            int top = 0;
+            int left = 0;
+            int right = 0;
+            int bottom = 0;
+            bool flag;
+            //top
+            flag = true;
+            for (int i = 0; i < pictureBox1.Height; i++)
+            {
+                if (flag == false)
+                {
+                    break;
+                }
+                for (int j = 0; j < pictureBox1.Width; j++)
+                {
+                    if (flag == false)
+                    {
+                        break;
+                    }
+                    if (bmp.GetPixel(j, i).R == 0 && bmp.GetPixel(j, i).G == 0 && bmp.GetPixel(j, i).B == 0)
+                    {
+                        top = i;
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            //left
+            flag = true;
+            for (int i = 0; i < pictureBox1.Width; i++)
+            {
+                if (flag == false)
+                {
+                    break;
+                }
+                for (int j = 0; j < pictureBox1.Height; j++)
+                {
+                    if (bmp.GetPixel(i, j).R == 0 && bmp.GetPixel(i, j).G == 0 && bmp.GetPixel(i, j).B == 0)
+                    {
+                        left = i;
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+   
+            //right
+            flag = true;
+            for (int i = pictureBox1.Width - 1; i >= 0; i--)
+            {
+                if (flag == false)
+                {
+                    break;
+                }
+                for (int j = pictureBox1.Height - 1; j >= 0; j--)
+                {
+                    if (bmp.GetPixel(i, j).R == 0 && bmp.GetPixel(i, j).G == 0 && bmp.GetPixel(i, j).B == 0)
+                    {
+                        right = i;
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            //bottom
+            flag = true;
+            for (int i = pictureBox1.Height - 1; i >= 0; i--)
+            {
+                if (flag == false)
+                {
+                    break;
+                }
+                for (int j = pictureBox1.Width - 1; j >= 0; j--)
+                {
+                    if (flag == false)
+                    {
+                        break;
+                    }
+                    if (bmp.GetPixel(j, i).R == 0 && bmp.GetPixel(j, i).G == 0 && bmp.GetPixel(j, i).B == 0)
+                    {
+                        bottom = i;
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            //----------------------------
+            //обнуляем матрицу
+            //----------------------------
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Xi[i,j] = 0;
+                }
+            }
+            //вырезаем часть картинки с символом
+            b2 = bmp.Clone(new Rectangle(left, top, right - left, bottom - top), bmp.PixelFormat);
+            pictureBox2.Image = b2;
+            //b2.Save("Test.bmp");
+            //составляем матрицу Xi
+            flag = false;
+            int h1 = b2.Width / N;
+            int h2 = b2.Height / N;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    flag = false;
+                    for (int x = (h1 * i); x < (h1 * (i + 1)); x++)
+                    {
+                        if (flag == true) break;
+                        for (int y = (h2 * j); y < (h2 * (j + 1)); y++)
+                        {
+                            if (b2.GetPixel(x, y).R == 0 && b2.GetPixel(x, y).G == 0 && b2.GetPixel(x, y).B == 0)
+                            {
+                                flag = true;
+                                Xi[j, i] = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            //перецептрон
+            double FS = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    FS += Xi[i, j] * Wi[i, j] - theta;
+                }
+            }
+            if ( FS >= 0 )
+            {
+                textBox1.Clear();
+                textBox1.Text = "2";
+                y1 = 1;
+            }
+            else
+            {
+                textBox1.Clear();
+                textBox1.Text = "7";
+                y1 = 0;
+            }
+            textBox1.Text += " " + FS.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double tmp = 0;
+            double n = 0.5;
+            if (y1 == 1)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        tmp = Wi[i, j] + n * -1 * Xi[i, j];
+                        Wi[i, j] = tmp;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        tmp = Wi[i, j] + n * 1 * Xi[i, j];
+                        Wi[i, j] = tmp;
+                    }
+                }
+            }
+            SAVE();
+        }
+    }
+}
